7ff1af366e2ae6902fe8eed1ff0ca8f09f7661b4
karlvoigt <360appleid@gmail.com>
2022-09-17 11:57:47 +0200

    Basic UI

--- lib/main.dart
+++ lib/main.dart
@@ -13,18 +13,9 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Hackathon 2022',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.blue,
+        primarySwatch: Colors.cyan,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: const MyHomePage(title: 'Cowrie Cash'),
     );
   }
 }
@@ -61,6 +52,8 @@ class _MyHomePageState extends State<MyHomePage> {
     });
   }
 
+  void _buttonTest() {}
+
   @override
   Widget build(BuildContext context) {
     // This method is rerun every time setState is called, for instance as done
@@ -73,43 +66,142 @@ class _MyHomePageState extends State<MyHomePage> {
       appBar: AppBar(
         // Here we take the value from the MyHomePage object that was created by
         // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
+        title: Center(
+            child: Text(
+                widget.title,
+                textAlign: TextAlign.center,
+              style: const TextStyle(
+                  fontSize: 30, color: Colors.white),
+            )
+        ),
       ),
       body: Center(
         // Center is a layout widget. It takes a single child and positions it
         // in the middle of the parent.
         child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
+          mainAxisAlignment: MainAxisAlignment.start,
           children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
-            ),
+            Container(
+                child: Column(
+              children: [
+                Container(
+                    margin: const EdgeInsets.all(10),
+                    height: 200,
+                    child: FractionallySizedBox(
+                        heightFactor: 0.4,
+                        alignment: FractionalOffset.center,
+                        child: AspectRatio(
+                            aspectRatio: 1 / 1,
+                            child: Placeholder(
+                              color: Colors.tealAccent,
+                            )))),
+              ],
+            )),
+            Container(
+                margin: const EdgeInsets.all(10),
+                height: 150,
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Container(
+                        margin: EdgeInsets.only(top: 20, left: 10, right: 10),
+                        child: AspectRatio(
+                            aspectRatio: 5 / 4,
+                            child: ElevatedButton(
+                                onPressed: _buttonTest,
+                                child: Column(
+                                    mainAxisAlignment: MainAxisAlignment.center,
+                                    children: [
+                                      Icon(
+                                        size: 50,
+                                        Icons.camera_alt_outlined,
+                                        color: Colors.white,
+                                      ),
+                                      Padding(
+                                          padding: EdgeInsets.only(top: 10)),
+                                      const Text(
+                                        'Send',
+                                        style: const TextStyle(
+                                            fontSize: 20, color: Colors.white),
+                                      )
+                                    ])))),
+                    Container(
+                        margin: EdgeInsets.only(top: 20, left: 10, right: 10),
+                        child: AspectRatio(
+                            aspectRatio: 5 / 4,
+                            child: ElevatedButton(
+                                onPressed: _buttonTest,
+                                child: Column(
+                                    mainAxisAlignment: MainAxisAlignment.center,
+                                    children: [
+                                      Icon(
+                                        size: 50,
+                                        Icons.qr_code_2_rounded,
+                                        color: Colors.white,
+                                      ),
+                                      Padding(
+                                          padding: EdgeInsets.only(top: 10)),
+                                      const Text(
+                                        'Receive',
+                                        style: const TextStyle(
+                                            fontSize: 20, color: Colors.white),
+                                      )
+                                    ])))),
+                  ],
+                )),
+            Container(
+                margin: const EdgeInsets.all(10),
+                height: 150,
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Container(
+                        margin: EdgeInsets.only(top:20,left: 10,right:10),
+                        child: AspectRatio(
+                            aspectRatio: 5/4,
+                            child: ElevatedButton(
+                                onPressed: _buttonTest,
+                                child: Column(
+                                    mainAxisAlignment:
+                                    MainAxisAlignment.center,
+                                    children: [
+                                      Icon(
+                                        size:50,
+                                        Icons.account_balance_outlined,
+                                        color: Colors.white,
+                                      ),
+                                      Padding(padding: EdgeInsets.only(top:10)),
+                                      const Text(
+                                        'Load',
+                                        style: const TextStyle(fontSize: 20, color: Colors.white),
+                                      )
+                                    ])))),
+                    Container(
+                        margin: EdgeInsets.only(top:20,left: 10,right:10),
+                        child: AspectRatio(
+                            aspectRatio: 5/4,
+                            child: ElevatedButton(
+                                onPressed: _buttonTest,
+                                child: Column(
+                                    mainAxisAlignment:
+                                    MainAxisAlignment.center,
+                                    children: [
+                                      Icon(
+                                        size:50,
+                                        Icons.account_box_outlined,
+                                        color: Colors.white,
+                                      ),
+                                      Padding(padding: EdgeInsets.only(top:10)),
+                                      const Text(
+                                        'Account',
+                                        style: const TextStyle(fontSize: 20, color: Colors.white),
+                                      )
+                                    ])))),
+                  ],
+                )),
           ],
         ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+      ),// This trailing comma makes auto-formatting nicer for build methods.
     );
   }
 }
