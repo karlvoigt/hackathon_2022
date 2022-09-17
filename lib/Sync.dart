3ad08530499ad3c016a655d012c8e0fae5674c4d
karlvoigt <360appleid@gmail.com>
2022-09-17 13:34:12 +0200

    Firebase working

---
+++ lib/Sync.dart
@@ -0,0 +1,27 @@
+class syncData{
+  String userIdSend = '';
+  String userIdReceive = '';
+  String userNameReceive = '';
+  int Balance = 0;
+  int updateAmount = 0;
+
+  void getBalance(){
+
+  }
+
+  void updateBalance(String userId, int amount){
+
+  }
+
+  void getUserSend(){
+
+  }
+
+  void getUserReceive(){
+
+  }
+
+
+
+
+}
